5657ec7b28838cc796993f126b3e43e3a1896548
Mimi C <mina123fire@yahoo.com>
2020-09-25 23:54:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,13 +43,13 @@ def valid_move?(board, index)
 end
 
 def turn(board)
-  
+  /*puts "Please enter 1-9:"*/
   input=input_to_index(gets.strip)
   if valid_move?(board, input)
     move(board, input, current_player(board))
     
   else
-    
+    /*puts "Please try again with 1-9:"*/
     turn(board)
   end
 end
@@ -127,7 +127,7 @@ end
 
 def play(board)
   if won?(board)
-    puts "Congratulations " + winner(board) + "!"
+    puts "Congratulations " + current_player(board) + "!"
   elsif draw?(board)
     puts "Cat's Game!"
   elsif !(over?(board))
